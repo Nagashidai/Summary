ddae692f9cc3f13a4b3ef6d71bfda42c9d5abfe7
Nagashidai <46490138+Nagashidai@users.noreply.github.com>
2019-03-24 13:07:30 +0900

    Add files via upload

---
+++ HTMLCSS/view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>PHPのテスト</title>
+</head>
+<body>
+
+<?php
+echo $_POST["onamae"]."さん、こんにちは！";
+echo "<br><br>";
+echo "本文：<br>";
+echo $_POST["honbun"]
+?>
+
+</body>
+</html>
